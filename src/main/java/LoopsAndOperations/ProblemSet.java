5e68780c091f960ba63526a5bde4e72faef1e80d
markoka101 <markoka101@gmail.com>
2024-08-27 17:07:08 -0400

    added problem set

---
+++ src/main/java/LoopsAndOperations/ProblemSet.java
@@ -0,0 +1,33 @@
+package LoopsAndOperations;
+
+public class ProblemSet {
+
+    /*
+    Calculate and return the result of x and y using the given operation
+    operations: + - * /
+    */
+    public int calculate(char op, int x, int y) {
+        return 0;
+    }
+
+    /*
+    Return F(n) in the Fibonacci sequence
+     */
+    public int fib(int n) {
+        return 0;
+    }
+
+    /*
+    Set index of array to either fizz, buzz, fizzbuzz, or number depending on the number
+    Range will be 0 <= i < n
+    fizz: divisible by 3
+    buzz: divisible by 5
+    fizzbuzz: divisible by both 3 and 5
+
+    Return the String array
+     */
+    public String[] fizzBuzz(int n) {
+        String[] result = new String[n];
+        return result;
+    }
+}
